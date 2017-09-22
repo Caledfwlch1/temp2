ce5f4e00a757ab1edaffb2caf5b9c4e787dd2ed6
andy <caledfwlch1@gmail.com>
2017-09-22 15:35:04 +0300

    new pack

--- run2/dbdelays_test.go
+++ run2/dbdelays_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestDbdelays(t *testing.T) {
-	//Run2("http://35.190.167.185/custom?branch=dev&commit=6c910dffc96d6a4d0c5a32fc6f53bc4f9d050570&roles=dbdelays&wait=true")
-	Run2("http://localhost/")
+	Run2("http://35.190.167.185/custom?branch=dev&commit=6c910dffc96d6a4d0c5a32fc6f53bc4f9d050570&roles=dbdelays&wait=true")
+	//Run2("http://localhost/")
 	return
 }
