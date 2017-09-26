8cd83f4521e1fab0de3ba97669873d25fbddcbe5
andy <caledfwlch1@gmail.com>
2017-09-26 13:25:42 +0300

    new pack

--- run2/failed_nodes_test.go
+++ run2/failed_nodes_test.go
@@ -2,10 +2,12 @@ package run2
 
 import (
 	"testing"
+	"time"
 )
 
 func TestFailed_nodes(t *testing.T) {
-	Run2("http://35.190.167.185/custom?branch=dev&commit=e43c8b290fb575808b8aafb680a45eeeb2221282&roles=failed_nodes&wait=true")
-	//Run2("http://localhost/")
+	//Run2("http://35.190.167.185/custom?branch=dev&commit=e43c8b290fb575808b8aafb680a45eeeb2221282&roles=failed_nodes&wait=true")
+	time.Sleep(time.Minute * 11)
+	Run2("http://localhost/")
 	return
 }
